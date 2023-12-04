94b1c8aedef4d3657ef0659f25363a0a93ec423e
komiljon_dev <turayevkomiljon@gmail.com>
2023-12-04 12:56:17 +0500

    Initial commit

---
+++ customdialog/src/main/java/com/example/customdialog/CustomDialog.kt
@@ -0,0 +1,15 @@
+package com.example.customdialog
+
+import android.content.Context
+import androidx.appcompat.app.AlertDialog
+
+object CustomDialog {
+
+    fun createInfoDialog(context: Context,title:String,message:String){
+        val alertDialog = AlertDialog.Builder(context)
+        alertDialog.setTitle(title)
+        alertDialog.setMessage(message)
+        alertDialog.show()
+
+    }
+}
